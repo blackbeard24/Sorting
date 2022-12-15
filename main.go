dce22b525dbe2279839539066087f34649cf69a2
dennis <you@example.com>
2022-12-15 14:17:55 -0500

    Second commit:- Selection Sort

--- main.go
+++ main.go
@@ -14,14 +14,18 @@ func main() {
 	fmt.Println(arr)
 
 	bubble(arr)
+	selection(arr)
 }
 
 func bubble(arr [7]int) {
 	//Bubble Sort
 	swap := true
-	for !swap {
+	//count := 0
+	for swap {
+		//count++
 		swap = false
 		for i := 0; i < 6; i++ {
+			//count++
 			if arr[i] > arr[i+1] {
 				arr[i], arr[i+1] = arr[i+1], arr[i]
 				swap = true
@@ -31,4 +35,27 @@ func bubble(arr [7]int) {
 
 	fmt.Println("Bubble Sort Result:-")
 	fmt.Println(arr)
+	//fmt.Println(count)
+}
+
+func selection(arr [7]int) {
+	//Selection Sort
+	//count := 0
+	for i := 0; i < 6; i++ {
+		//count++
+		minIndex := i
+		for j := i + 1; j < 7; j++ {
+			//count++
+			if arr[j] < arr[minIndex] {
+				minIndex = j
+			}
+
+		}
+		if minIndex != i {
+			arr[i], arr[minIndex] = arr[minIndex], arr[i]
+		}
+	}
+	fmt.Println("Selection Sort Result:-")
+	fmt.Println(arr)
+	//fmt.Println(count)
 }
